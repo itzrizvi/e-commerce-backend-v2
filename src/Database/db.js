a9b778655c4f215be5686452de840eb9498a8f7f
itzrizvi <shahriar.me.work@gmail.com>
2022-09-06 18:04:13 +0600

    Database Configuration Done

--- src/Database/db.js
+++ src/Database/db.js
@@ -2,12 +2,13 @@ const pg = require('pg');
 const Pool = pg.Pool;
 const { Client } = require('pg');
 
-//
+// DB CONFIGS
 const database = process.env.DB_NAME;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 const host = process.env.HOST;
 
+// SETTING UP DB CONFIGS
 const localPoolConfig = {
     user: user,
     password: password,
@@ -18,6 +19,7 @@ const localPoolConfig = {
     min: 5
 }
 
+// CONFIGURE POOL AND CLIENT
 const pool = new Pool(localPoolConfig);
 const clientConnect = new Client(localPoolConfig);
 

44682859ac0585a5dc956b4feaf4d01af2d27ea3
itzrizvi <shahriar.me.work@gmail.com>
2022-09-15 04:36:52 +0600

    Updated Email verification Controller

--- src/controllers/users/emailVerificationController.js
+++ src/controllers/users/emailVerificationController.js
@@ -1,22 +1,23 @@
+// All Requires For Email Verification
 const { verifyEmail } = require("../../helpers/userHelper");
 const { emailVerifyRequest } = require("../../requests/emailVerifyRequests");
 const { singleResponse } = require("../../utils/response");
 
 
 
-
+// Exports
 module.exports = async (req, db, user, isAuth) => {
 
-    //
+    // Validate Request
     const validate = await emailVerifyRequest(req);
     if (!validate.success) {
         return singleResponse(validate.data);
     }
 
-    //
+    // Verify Email With Helper
     const data = await verifyEmail(req, db, user, isAuth);
 
-    //
+    // Final Response
     return singleResponse(data);
 
 }

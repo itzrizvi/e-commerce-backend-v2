ca75fac7b14b4566f402fdc9b5545fe1d59a240d
Md.Samrat Shahajahan <sumonskys@gmail.com>
2022-10-12 17:48:52 +0600

    AWS New Requirement

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -2,13 +2,12 @@ const {parse, join} = require("path"); // This is node built in package
 const {createWriteStream} = require("fs"); // this is node built in package
 const fs = require("fs"); // this is node built in package
 require('dotenv').config()
+const config = require('config');
 const S3 = require('aws-sdk/clients/s3')
 const util = require('util')
 const unlinkFile = util.promisify(fs.unlink)
 
-const readBucketName = process.env.AWS_READ_BUCKET_NAME
-const writeBucketName = process.env.AWS_WRITE_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
+const region = config.get('AWS.BUCKET_REGION');
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
 
@@ -18,19 +17,19 @@ const s3 = new S3({
     secretAccessKey
   })
 
-module.exports.singleFileUpload = async (file, folder = '', fn = '') => {
+module.exports.singleFileUpload = async ({ file, folder = '', idf, fileName = '', bucketName }) => {
     const {createReadStream, filename} = await file;
     const stream = createReadStream();
-    var {ext, name} = parse(filename);
-    if(fn == '') name = `${Math.floor((Math.random() * 10000) + 1)}`;
-    else name = fn
-    const fileName = `${name}-${Date.now()}${ext}`
-    let url = join(__dirname, `../../tmp/${fileName}`);
+    var {ext} = parse(filename);
+    if(fileName == '') imageName = `${Math.floor((Math.random() * 10000) + 1)}`;
+    else imageName = fileName
+    const fileNameExt = imageName + ext
+    let url = join(__dirname, `../../tmp/${fileNameExt}`);
     const imageStream = createWriteStream(url)
     await stream.pipe(imageStream);
-    const folderName = folder == '' ? fileName : `${folder}/${fileName}`
+    const folderName = folder == '' ? fileNameExt : `${folder}/${idf}/${fileNameExt}`
     const uploadParams = {
-        Bucket: writeBucketName,
+        Bucket: bucketName,
         Body: stream,
         Key: folderName
     }
@@ -39,21 +38,21 @@ module.exports.singleFileUpload = async (file, folder = '', fn = '') => {
     return upload
 } // This is single readfile
 
-module.exports.multipleFileUpload = async (file, folder= '', fn = '') => {
+module.exports.multipleFileUpload = async ({ file, folder = '',idf, fileName = '', bucketName }) => {
     let fileUrl = [];
     for (let i = 0; i < file.length; i++) {
         const {createReadStream, filename} = await file[i];
         const stream = createReadStream();
-        var {ext, name} = parse(filename);
-        if(fn == '') name = `${Math.floor((Math.random() * 10000) + 1)}`;
-        else name = fn
-        const fileName = `${name}-${Date.now()}${ext}`
-        let url = join(__dirname, `../../tmp/${fileName}`);
+        var {ext} = parse(filename);
+        if(fileName == '') imageName = `${Math.floor((Math.random() * 10000) + 1)}`;
+        else imageName = fileName
+        const fileNameExt = `${imageName}-${Date.now()}${ext}`
+        let url = join(__dirname, `../../tmp/${fileNameExt}`);
         const imageStream = createWriteStream(url)
         await stream.pipe(imageStream);
-        const folderName = folder == '' ?  fileName : `${folder}/${fileName}`
+        const folderName = folder == '' ?  fileNameExt : `${folder}/${idf}/${fileNameExt}`
         const uploadParams = {
-            Bucket: writeBucketName,
+            Bucket: bucketName,
             Body: stream,
             Key: folderName
         }
@@ -68,10 +67,30 @@ module.exports.multipleFileUpload = async (file, folder= '', fn = '') => {
   async function getFileStream(req, res) {
 
     const key = req.params[0]
+    const type = key.split('/')[0]
+    let bucketName;
+    
+    switch (type) {
+      case 'brand':
+        bucketName = config.get("AWS.BRAND_IMG_DEST").split("/")[0]
+        break;
+      case 'product':
+        bucketName = config.get("AWS.PRODUCT_IMG_THUMB_DEST").split("/")[0]
+        break;
+      case 'category':
+        bucketName = config.get("AWS.CATEGORY_IMG_DEST").split("/")[0]
+        break;
+      case 'user':
+        bucketName = config.get("AWS.USER_IMG_DEST").split("/")[0]
+        break;
+      default:
+        bucketName = config.get("AWS.READ_BUCKET")
+        break;
+    }
 
     const downloadParams = {
       Key: key,
-      Bucket: readBucketName
+      Bucket: bucketName
     }
 
     // Using callbacks
@@ -86,22 +105,19 @@ module.exports.multipleFileUpload = async (file, folder= '', fn = '') => {
 
 
 // Delete Single file from aws
-  module.exports.deleteFile = (folder, key, ext) => {
-    // const uploadParams = {
-    //     Bucket: writeBucketName,
-    //     Key: folder + key + "." + ext
-    //   }
-    // s3.deleteObject(uploadParams).promise()
+  module.exports.deleteFile = ({fileName, folder, idf, bucketName}) => {
+
     const uploadParams2 = {
-      Bucket: readBucketName,
-      Key: folder + key + "." + ext
+      Bucket: bucketName,
+      Key: `${folder}/${idf}/${fileName}`
     }
     s3.deleteObject(uploadParams2).promise()
-    const sizes = ['128x128', '400x400', '800x800', '1200x1200']
+
+    const sizes = config.get("AWS.IMAGE_SIZE")
     sizes.forEach( (size) => {
       const uploadParams3 = {
-        Bucket: readBucketName,
-        Key: folder + size + '_' + key  + "." + ext
+        Bucket: bucketName,
+        Key: `${folder}/${idf}/${size}_${fileName}`
       }
       s3.deleteObject(uploadParams3).promise()
     })
@@ -109,51 +125,37 @@ module.exports.multipleFileUpload = async (file, folder= '', fn = '') => {
   }
 
   // Delete Multiple file
-  module.exports.deleteFiles = (files) => {
-    // const uploadParams = {
-    //     Bucket: writeBucketName,
-    //     Delete:{
-    //       Objects:[]
-    //     }
-    //   }
-    //   files.forEach((object) => {
-    //       uploadParams.Delete.Objects.push({
-    //         Key: object.folder + object.key + "." + object.ext
-    //       })
-    //   });
-    // s3.deleteObjects(uploadParams).promise()
-
-
-    const uploadParams2 = {
-      Bucket: readBucketName,
+  module.exports.deleteFiles = ({filesName, folder, idf, bucketName}) => {
+    const uploadParams = {
+      Bucket: bucketName,
       Delete:{
         Objects:[]
       }
     }
-    files.forEach((object) => {
-      uploadParams2.Delete.Objects.push({
-          Key: object.folder + object.key + "." + object.ext
+    filesName.forEach((object) => {
+      uploadParams.Delete.Objects.push({
+          Key:  `${folder}/${idf}/${object}`
         })
     });
-    s3.deleteObjects(uploadParams2).promise()
+    s3.deleteObjects(uploadParams).promise()
 
-    const sizes = ['128x128', '400x400', '800x800', '1200x1200']
+    const sizes = config.get("AWS.IMAGE_SIZE")
     sizes.forEach( (size) => {
-      const uploadParams3 = {
-        Bucket: readBucketName,
+
+      const uploadParams2 = {
+        Bucket: bucketName,
         Delete:{
           Objects:[]
         }
       }
-
-      files.forEach((object) => {
-        uploadParams3.Delete.Objects.push({
-            Key: object.folder + size + '_' + object.key  + "." + object.ext
+      filesName.forEach((object, index) => {
+        uploadParams2.Delete.Objects.push({
+            Key: `${folder}/${idf}/${size}_${object}`
           })
       });
 
 
-      s3.deleteObjects(uploadParams3).promise()
+      s3.deleteObjects(uploadParams2).promise()
     })
 
     return;

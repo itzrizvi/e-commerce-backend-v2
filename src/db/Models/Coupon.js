ff304dc997123e92139cc5a4dfcaab3659e41f40
itzrizvi <shahriar.me.work@gmail.com>
2022-10-18 22:13:42 +0600

    Coupon Table and Model Added

---
+++ src/db/Models/Coupon.js
@@ -0,0 +1,67 @@
+// Coupon Model
+module.exports = (sequelize, DataTypes) => {
+
+    const Coupon = sequelize.define("coupons", {
+        coupon_uuid: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        coupon_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        coupon_code: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        coupon_description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        coupon_type: {
+            type: DataTypes.ENUM("flat", "percentage"),
+            allowNull: false
+        },
+        coupon_amount: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        coupon_maxamount: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        coupon_minamount: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        coupon_startdate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        coupon_enddate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        coupon_status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        coupon_sortorder: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true,
+        freezeTableName: true,
+        tableName: 'coupons',
+    })
+
+    return Coupon
+}

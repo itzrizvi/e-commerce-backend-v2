e31e413cb38e43b7b9907eaa2c623e50a6ba5a40
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 21:07:59 +0600

    Permission Checked Added in Manufacture or Brand Controller

--- src/controllers/brands/createBrandController.js
+++ src/controllers/brands/createBrandController.js
@@ -1,11 +1,20 @@
 // ALL REQUIRES
 const { createBrand } = require("../../helpers/brandHelper");
 const { createBrandRequest } = require("../../requests/brandRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "manufacture";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Create Brand Request
     const validate = await createBrandRequest(req);
     if (!validate.success) {

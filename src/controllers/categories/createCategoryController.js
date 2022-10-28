a360c4784bc313944f305e42075db546067df58c
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:43:24 +0600

    Permission Checked Added in Category Controller

--- src/controllers/categories/createCategoryController.js
+++ src/controllers/categories/createCategoryController.js
@@ -1,10 +1,20 @@
 // All Requires
 const { createCategory } = require("../../helpers/categoryHelper");
 const { createCategoryRequest } = require("../../requests/categoryRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 // Create Category Controller
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+
+    // Permission Name of this API
+    const permissionName = "category";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
 
     // Validate Create Role Request
     const validate = await createCategoryRequest(req);

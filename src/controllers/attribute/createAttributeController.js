cc979f42b83ee3da6b99a2825b13ee3797183697
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:39:10 +0600

    Permission Checked Added in Attribute Controller

--- src/controllers/attribute/createAttributeController.js
+++ src/controllers/attribute/createAttributeController.js
@@ -1,11 +1,20 @@
 // ALL REQUIRES;
 const { createAttribute } = require("../../helpers/attributeHelper");
 const { createAttributeRequest } = require("../../requests/attributeRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "attribute";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Create Attr Request
     const validate = await createAttributeRequest(req);
     if (!validate.success) {
--- src/controllers/attribute/updateAttributeController.js
+++ src/controllers/attribute/updateAttributeController.js
@@ -2,11 +2,20 @@
 
 const { updateAttribute } = require("../../helpers/attributeHelper");
 const { updateAttributeRequest } = require("../../requests/attributeRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "attribute";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Update Attr Request
     const validate = await updateAttributeRequest(req);
     if (!validate.success) {

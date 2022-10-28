a660712c4f0ccba86b699ffacad0a182f2eb5c17
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:58:50 +0600

    Permission Checked Added in Customer Controller

--- src/controllers/customer/updateCustomerShippingAddressController.js
+++ src/controllers/customer/updateCustomerShippingAddressController.js
@@ -1,9 +1,18 @@
 const { updateCustomerShippingAddress } = require("../../helpers/customerHelper");
 const { updateCustomerShippingAddressRequest } = require("../../requests/customerRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "customer";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Create Brand Request
     const validate = await updateCustomerShippingAddressRequest(req);
     if (!validate.success) {

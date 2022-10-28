73d5c4a2180b0f535040dc1d3ab7a8c4ec8a3dd9
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:41:18 +0600

    Permission Checked Added in Attribute Group Controller

--- src/controllers/attributeGroup/createAttrGroupController.js
+++ src/controllers/attributeGroup/createAttrGroupController.js
@@ -1,11 +1,20 @@
 // ALL REQUIRES;
 const { createAttrGroup } = require("../../helpers/attributeGroupHelper");
 const { createAttrGroupRequest } = require("../../requests/attributeRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "attribute-group";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Create Attr Group Request
     const validate = await createAttrGroupRequest(req);
     if (!validate.success) {
--- src/controllers/attributeGroup/updateAttrGroupController.js
+++ src/controllers/attributeGroup/updateAttrGroupController.js
@@ -1,11 +1,21 @@
 // ALL REQUIRES;
 const { updateAttrGroup } = require("../../helpers/attributeGroupHelper");
 const { updateAttrGroupRequest } = require("../../requests/attributeRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "attribute-group";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
+
     // Validate Update Attr Group Request
     const validate = await updateAttrGroupRequest(req);
     if (!validate.success) {

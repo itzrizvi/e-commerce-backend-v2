ba48f30ce229945b48327205db41aaa3dbec28af
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:25:30 +0600

    Permission Checked Added in Roles Permission Controller

--- src/controllers/rolesPermission/createRolesPermissionController.js
+++ src/controllers/rolesPermission/createRolesPermissionController.js
@@ -1,13 +1,20 @@
 // ALL REQUIRES
 const { createRolesPermission } = require("../../helpers/rolesPermissionHelper");
 const { createRolesPermissionRequest } = require("../../requests/rolesPermissionRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
-
-
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+
+    // Permission Name of this API
+    const permissionName = "permission";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
 
     // Validate Create Role Request
     const validate = await createRolesPermissionRequest(req);
--- src/controllers/rolesPermission/updateRolesPermissionController.js
+++ src/controllers/rolesPermission/updateRolesPermissionController.js
@@ -1,6 +1,7 @@
 // ALL REQUIRES
 const { updateRolesPermission } = require("../../helpers/rolesPermissionHelper");
 const { updateRolesPermissionRequest } = require("../../requests/rolesPermissionRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
@@ -8,6 +9,14 @@ const { singleResponse } = require("../../utils/response");
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+
+    // Permission Name of this API
+    const permissionName = "permission";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
 
     // Validate Update Roles Permission Request
     const validate = await updateRolesPermissionRequest(req);

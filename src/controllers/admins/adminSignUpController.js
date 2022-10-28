987812d131a336bec5c4d4b6e0776cde5805d816
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 20:36:14 +0600

    Permission Checked Added in Roles Admin User Controller

--- src/controllers/admins/adminSignUpController.js
+++ src/controllers/admins/adminSignUpController.js
@@ -1,12 +1,23 @@
 // All Requires
 const { adminSignUp } = require("../../helpers/adminHelper");
 const { adminSignUpRequest } = require("../../requests/adminRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+
+    // Permission Name of this API
+    const permissionName = "user";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
+
     // Validate Data
     const validate = await adminSignUpRequest(req);
     if (!validate.success) {

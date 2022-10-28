70d8be3a6da05d5e4fdbcdd1ed65831da1f4304e
itzrizvi <shahriar.me.work@gmail.com>
2022-10-28 21:03:40 +0600

    Permission Checked Added in Customer Group Controller

--- src/controllers/customerGroup/createCustomerGroupController.js
+++ src/controllers/customerGroup/createCustomerGroupController.js
@@ -1,11 +1,20 @@
 // ALL REQUIRES;
 const { createCustomerGroup } = require("../../helpers/customerGroupHelper");
 const { createCustomerGroupGroupRequest } = require("../../requests/customerGroupRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "customer-group";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Create Customer Group Request
     const validate = await createCustomerGroupGroupRequest(req);
     if (!validate.success) {
--- src/controllers/customerGroup/updateCustomerGroupController.js
+++ src/controllers/customerGroup/updateCustomerGroupController.js
@@ -1,11 +1,20 @@
 // ALL REQUIRES;
 const { updateCustomerGroup } = require("../../helpers/customerGroupHelper");
 const { updateCustomerGroupRequest } = require("../../requests/customerGroupRequests");
+const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
+    // Permission Name of this API
+    const permissionName = "customer-group";
+    // Check Permission
+    const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+    if (!checkPermissions.success) {
+        return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+    }
+
     // Validate Update Attr Group Request
     const validate = await updateCustomerGroupRequest(req);
     if (!validate.success) {

bbf03f3d0cdc72f7d0a9a83d7830ef7f84f32de0
itzrizvi <shahriar.me.work@gmail.com>
2022-10-31 18:36:37 +0600

    Category id conversion done

--- src/controllers/categories/updateCategoryController.js
+++ src/controllers/categories/updateCategoryController.js
@@ -1,6 +1,5 @@
 // All Requires
 const { updateCategory } = require("../../helpers/categoryHelper");
-const { updateCategoryRequest } = require("../../requests/categoryRequests");
 const { checkPermission } = require("../../utils/permissionChecker");
 const { singleResponse } = require("../../utils/response");
 
@@ -12,12 +11,6 @@ module.exports = async (req, db, user, isAuth, TENANTID) => {
     const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
     if (!checkPermissions.success) {
         return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
-    }
-
-    // Validate Update Category Request
-    const validate = await updateCategoryRequest(req);
-    if (!validate.success) {
-        return singleResponse(validate.data);
     }
 
     // Helper

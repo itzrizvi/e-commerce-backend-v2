d4f06b9e8f08a788003a3c48a3b1058f46daa0a5
SuMoN <sumonskys@gmail.com>
2022-11-01 15:38:47 +0600

    table design

---
+++ src/db/Models/CartItem.js
@@ -0,0 +1,37 @@
+// Address model
+module.exports = (sequelize, DataTypes) => {
+
+    const CartItem = sequelize.define("cart_item", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        product_id: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        createdBy: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updatedBy: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return CartItem
+}
---
+++ src/db/Models/Payment.js
@@ -0,0 +1,40 @@
+// Address model
+module.exports = (sequelize, DataTypes) => {
+
+    const Payment = sequelize.define("payment", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status:{
+            type: DataTypes.BOOLEAN
+        },
+        createdBy: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updatedBy: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return Payment
+}

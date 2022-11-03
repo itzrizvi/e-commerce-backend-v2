13b6b54bb0bde488c5c9f09773317914f922b52a
itzrizvi <shahriar.me.work@gmail.com>
2022-11-03 21:08:14 +0600

    Order Status Table and Model Done

---
+++ src/db/Models/OrderStatus.js
@@ -0,0 +1,41 @@
+// Order Status model
+module.exports = (sequelize, DataTypes) => {
+
+    const OrderStatus = sequelize.define("order_status", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return OrderStatus
+}

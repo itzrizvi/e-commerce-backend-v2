890fede1792e8601f8a66e651f57e6ba4dbb22ed
itzrizvi <shahriar.me.work@gmail.com>
2022-11-13 18:45:56 +0600

    Add Wish List API Done

--- src/helpers/wishListHelper.js
+++ src/helpers/wishListHelper.js
@@ -11,7 +11,37 @@ module.exports = {
         try {
 
             // DATA FROM REQUEST
-            console.log(req)
+            const { product_id } = req;
+
+            // Check If the wish list is already there
+            const checkExistWishList = await db.wishlist.findOne({
+                where: {
+                    [Op.and]: [{
+                        user_id: user.id,
+                        product_id,
+                        tenant_id: TENANTID
+                    }]
+                }
+            });
+
+            if (checkExistWishList) return { message: "You Already Have This Product on Your WishList!!!", status: false }
+
+            // Insert Wish List
+            const insertToWishlist = await db.wishlist.create({
+                user_id: user.id,
+                product_id,
+                tenant_id: TENANTID,
+                created_by: user.id
+            });
+
+            if (!insertToWishlist) return { message: "Wish List Couldn't Be Created!!!", status: false }
+
+            return {
+                message: "Successfully Added The Product To Your Wishlist!!!",
+                status: true,
+                tenant_id: TENANTID
+            }
+
 
 
         } catch (error) {

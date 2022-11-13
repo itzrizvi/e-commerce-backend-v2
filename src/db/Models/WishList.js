4cd9115fb2e3103f0e5a7e76cc2f35b7f1d8d926
itzrizvi <shahriar.me.work@gmail.com>
2022-11-13 17:05:37 +0600

    Wish List Table and Model done

---
+++ src/db/Models/WishList.js
@@ -0,0 +1,37 @@
+// Wish List model
+module.exports = (sequelize, DataTypes) => {
+
+    const WishList = sequelize.define("wishlist", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        product_id: {
+            type: DataTypes.BIGINT,
+            allowNull: true
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return WishList
+}

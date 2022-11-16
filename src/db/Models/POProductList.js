7b5f9996fbcfc1c99553ca1e9309ff62fc7960ce
itzrizvi <shahriar.me.work@gmail.com>
2022-11-16 19:59:15 +0600

    PO Product List Model and Table Done

---
+++ src/db/Models/POProductList.js
@@ -0,0 +1,59 @@
+// Purchase Order model
+module.exports = (sequelize, DataTypes) => {
+
+    const POProductList = sequelize.define("po_productlist", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        purchase_order_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        product_id: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        totalPrice: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        recieved_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        remaining_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return POProductList
+}

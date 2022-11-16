60093d2224a393ada37e2adb192cfa5514af4ce8
itzrizvi <shahriar.me.work@gmail.com>
2022-11-16 18:35:23 +0600

    Purchase Order Setting Model and Table Done

---
+++ src/db/Models/POSetting.js
@@ -0,0 +1,37 @@
+// Purchase Order Setting model
+module.exports = (sequelize, DataTypes) => {
+
+    const POSetting = sequelize.define("po_setting", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        po_prefix: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        po_startfrom: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return POSetting
+}

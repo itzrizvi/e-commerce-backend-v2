b6ae38fba7e750e3e6f77bee7296ace38523bd48
itzrizvi <shahriar.me.work@gmail.com>
2022-11-21 14:25:14 +0600

    Shipping method model and Table done

---
+++ src/db/Models/ShippingMethod.js
@@ -0,0 +1,49 @@
+// Shipping Method model
+module.exports = (sequelize, DataTypes) => {
+
+    const ShippingMethod = sequelize.define("shipping_method", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
+        shipping_cost: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        timestamps: true
+    })
+
+    return ShippingMethod
+}

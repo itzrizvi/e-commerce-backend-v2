ce2d8e2ae39977c4dcb58c1c4fe0c985bd76236f
itzrizvi <shahriar.me.work@gmail.com>
2022-12-07 22:23:34 +0600

    Permission Added on Customer Adderess APIS

--- src/controllers/customer/addCustomerShippingAddressController.js
+++ src/controllers/customer/addCustomerShippingAddressController.js
@@ -4,13 +4,15 @@ const { singleResponse } = require("../../utils/response");
 
 // CONTROLLER
 module.exports = async (req, db, user, isAuth, TENANTID) => {
-    // // Permission Name of this API
-    // const permissionName = "customer";
-    // // Check Permission
-    // const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
-    // if (!checkPermissions.success) {
-    //     return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
-    // }
+    if (user.has_role === '1') {
+        // Permission Name of this API
+        const permissionName = "customer";
+        // Check Permission
+        const checkPermissions = await checkPermission(db, user, TENANTID, permissionName);
+        if (!checkPermissions.success) {
+            return { message: "You dont have access to this route, please contact support to have you give this route permission!!!", status: false };
+        }
+    }
 
     // CREATE ROLE
     const data = await addCustomerShippingAddress(req, db, user, isAuth, TENANTID);

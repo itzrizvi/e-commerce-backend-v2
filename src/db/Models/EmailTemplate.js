9e3bbe2a83f00b48c0f401d9e781757db3b3e3d3
itzrizvi <shahriar.me.work@gmail.com>
2022-12-07 20:53:26 +0600

    All Email Template Table and Model Done

---
+++ src/db/Models/EmailTemplate.js
@@ -0,0 +1,49 @@
+// Email Header and Footer model
+module.exports = (sequelize, DataTypes) => {
+
+    const EmailTemplate = sequelize.define("email_template", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        header_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        footer_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        tenant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+    return EmailTemplate
+}

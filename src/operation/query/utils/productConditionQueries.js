25058aa78cc6a2ba1a2f6d445bee721c692e6577
itzrizvi <shahriar.me.work@gmail.com>
2022-12-28 13:50:19 +0600

    GET Product Condition API Public

--- src/operation/query/utils/productConditionQueries.js
+++ src/operation/query/utils/productConditionQueries.js
@@ -5,9 +5,9 @@ module.exports = {
     getAllProductCondition: async (root, args, { db, user, isAuth, TENANTID }, info) => {
         // Return If Not Have TENANT ID
         if (!TENANTID) return { message: "TENANT ID IS MISSING!!!", status: false }
-        // Return If No Auth
-        if (!user || !isAuth) return { message: "Not Authorized", status: false };
-        if (user.has_role === '0') return { message: "Not Authorized", status: false };
+        // // Return If No Auth
+        // if (!user || !isAuth) return { message: "Not Authorized", status: false };
+        // if (user.has_role === '0') return { message: "Not Authorized", status: false };
 
         // Return To Controller
         return await allProductConditionController(db, TENANTID);

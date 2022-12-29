81feeafe7674aba065f35ca8270498e1271cce06
Md.Samrat Shahajahan <sumonskys@gmail.com>
2022-12-29 23:33:04 +0600

    Payment Method modification

--- src/graphql/typeDefs/paymentTypes.js
+++ src/graphql/typeDefs/paymentTypes.js
@@ -9,7 +9,6 @@ module.exports = gql`
 type PaymentMethod {
     id:Int
     name:String
-    slug:String
     description:String
     status:Boolean
     tenant_id:String
@@ -50,7 +49,6 @@ type GetPaymentMethodListAdmin {
 type PaymentMethodPublic {
     id:Int!
     name:String!
-    slug:String!
     description:String!
     status:Boolean!
     isDefault: Boolean

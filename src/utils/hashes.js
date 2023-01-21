bd9990bc4d1c73cb124fab60ae5c002d4a89cb47
Samrat Shahajahan <sumonskys@gmail.com>
2023-01-21 21:12:11 +0600

    crypto Package Change and modify utils function

--- src/utils/hashes.js
+++ src/utils/hashes.js
@@ -1,25 +1,39 @@
-const CryptoJS = require("crypto-js");
+// const CryptoJS = require("crypto-js");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr(process.env.secretKey, {
+  pbkdf2Iterations: 10000,
+  saltLength: 10,
+});
+
+// const crypt = (text) => {
+
+//     let ciphertext = CryptoJS.AES.encrypt(text, process.env.secretKey).toString();
+//     let encodeURI = encodeURIComponent(ciphertext);
+
+//     return encodeURI
+// };
+
+// const decrypt = (encoded) => {
+
+//     let decodeURI = decodeURIComponent(encoded);
+//     let bytes = CryptoJS.AES.decrypt(decodeURI, process.env.secretKey);
+//     let decodedValue = bytes.toString(CryptoJS.enc.Utf8);
+
+//     return decodedValue
+// }
+
 const crypt = (text) => {
-
-    let ciphertext = CryptoJS.AES.encrypt(text, process.env.secretKey).toString();
-    let encodeURI = encodeURIComponent(ciphertext);
-
-    return encodeURI
+  const encryptedString = cryptr.encrypt(text);
+  return encryptedString;
 };
 
 const decrypt = (encoded) => {
-
-    let decodeURI = decodeURIComponent(encoded);
-    let bytes = CryptoJS.AES.decrypt(decodeURI, process.env.secretKey);
-    let decodedValue = bytes.toString(CryptoJS.enc.Utf8);
-
-    return decodedValue
-}
-
-
+  const decryptedString = cryptr.decrypt(encoded);
+  return decryptedString;
+};
 
 // Module Export
 module.exports = {
-    crypt,
-    decrypt
-}
+  crypt,
+  decrypt,
+};

7c7214eb359bf5717c956a6d4223e369686df3ad
itzrizvi <shahriar.me.work@gmail.com>
2023-01-26 23:54:39 +0600

    PO Vendor View LINKs Model Modified

--- src/db/Models/POVendorViewLinks.js
+++ src/db/Models/POVendorViewLinks.js
@@ -2,6 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
 
     const POVendorViewLinks = sequelize.define("po_vendor_view_links", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
         vendor_id: {
             type: DataTypes.INTEGER,
             allowNull: false,

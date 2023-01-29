c62d4a5a9dc35fb5b4dceed5ce558dbbf457dc40
Samrat Shahajahan <sumonskys@gmail.com>
2023-01-29 21:19:06 +0600

    Email Attachment Config

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,103 +1,130 @@
 // All Requires
 const { Op } = require("sequelize");
-var nodemailer = require('nodemailer');
-var handlebars = require('handlebars');
+var nodemailer = require("nodemailer");
+var handlebars = require("handlebars");
 const db = require("../db");
 const logger = require("../../logger");
+const config = require("config");
+const default_mailer = config.get("DEFAULT_MAILER");
 
-const Mail = async (mail_address, subject, data, alias, tenant_id) => {
+const Mail = async (
+  mail_address,
+  subject,
+  data,
+  alias,
+  tenant_id,
+  attachments = []
+) => {
+  try {
+    if (default_mailer === "node_mailer") {
+      // Transport Creator From Nodemailer
+      const transport = nodemailer.createTransport({
+        service: "Gmail",
+        host: process.env.NODEMAILER_HOST,
+        port: process.env.NODEMAILER_PORT,
+        auth: {
+          user: process.env.NODEMAILER_USER,
+          pass: process.env.NODEMAILER_PASS,
+        }
+      });
 
-    try {
-
-
-        // Transport Creator From Nodemailer
-        const transport = nodemailer.createTransport({
-            service: "Gmail",
-            host: process.env.NODEMAILER_HOST,
-            port: process.env.NODEMAILER_PORT,
-            auth: {
-                user: process.env.NODEMAILER_USER,
-                pass: process.env.NODEMAILER_PASS
-            }
+      //
+      if (
+        !db.email_template_list.hasAlias("email_template") &&
+        !db.email_template_list.hasAlias("emailteamplate")
+      ) {
+        await db.email_template_list.hasOne(db.email_template, {
+          sourceKey: "email_template_id",
+          foreignKey: "id",
+          as: "emailteamplate",
         });
+      }
 
-        // 
-        if (!db.email_template_list.hasAlias('email_template') && !db.email_template_list.hasAlias('emailteamplate')) {
-            await db.email_template_list.hasOne(db.email_template, {
-                sourceKey: 'email_template_id',
-                foreignKey: 'id',
-                as: 'emailteamplate'
-            });
-        }
+      //
+      if (
+        !db.email_template.hasAlias("email_header_footer") &&
+        !db.email_template.hasAlias("templateHeader")
+      ) {
+        await db.email_template.hasOne(db.email_header_footer, {
+          sourceKey: "header_id",
+          foreignKey: "id",
+          as: "templateHeader",
+        });
+      }
 
-        // 
-        if (!db.email_template.hasAlias('email_header_footer') && !db.email_template.hasAlias('templateHeader')) {
-            await db.email_template.hasOne(db.email_header_footer, {
-                sourceKey: 'header_id',
-                foreignKey: 'id',
-                as: 'templateHeader'
-            });
-        }
+      //
+      if (
+        !db.email_template.hasAlias("email_header_footer") &&
+        !db.email_template.hasAlias("templateFooter")
+      ) {
+        await db.email_template.hasOne(db.email_header_footer, {
+          sourceKey: "footer_id",
+          foreignKey: "id",
+          as: "templateFooter",
+        });
+      }
 
-        // 
-        if (!db.email_template.hasAlias('email_header_footer') && !db.email_template.hasAlias('templateFooter')) {
-            await db.email_template.hasOne(db.email_header_footer, {
-                sourceKey: 'footer_id',
-                foreignKey: 'id',
-                as: 'templateFooter'
-            });
-        }
-
-        //
-        const gettemplate = await db.email_template_list.findOne({
+      //
+      const gettemplate = await db.email_template_list.findOne({
+        include: [
+          {
+            model: db.email_template,
+            as: "emailteamplate",
             include: [
-                {
-                    model: db.email_template, as: "emailteamplate",
-                    include: [
-                        { model: db.email_header_footer, as: "templateHeader" },
-                        { model: db.email_header_footer, as: "templateFooter" },
-                    ]
-                }
+              { model: db.email_header_footer, as: "templateHeader" },
+              { model: db.email_header_footer, as: "templateFooter" },
             ],
-            where: {
-                [Op.and]: [{
-                    slug: alias,
-                    tenant_id: tenant_id
-                }]
-            }
-        });
+          },
+        ],
+        where: {
+          [Op.and]: [
+            {
+              slug: alias,
+              tenant_id: tenant_id,
+            },
+          ],
+        },
+      });
 
-        // CONCATE TEMPLATE
-        const emailBody = gettemplate.emailteamplate.content;
-        const emailHeader = gettemplate.emailteamplate.templateHeader.content;
-        const emailFooter = gettemplate.emailteamplate.templateFooter.content;
-        const emailTemplate = emailHeader.concat(emailBody).concat(emailFooter);
+      // CONCATE TEMPLATE
+      const emailBody = gettemplate.emailteamplate.content;
+      const emailHeader = gettemplate.emailteamplate.templateHeader.content;
+      const emailFooter = gettemplate.emailteamplate.templateFooter.content;
+      const emailTemplate = emailHeader.concat(emailBody).concat(emailFooter);
 
-        var template = handlebars.compile(emailTemplate);
-        var htmlToSend = template(data);
-        var mailOptions = {
-            from: process.env.NODEMAILER_FROM_EMAIL + "<" + process.env.NODEMAILER_FROM_EMAIL + ">",
-            to: mail_address,
-            subject: subject,
-            html: htmlToSend
-        };
+      var template = handlebars.compile(emailTemplate);
+      var htmlToSend = template(data);
+      var mailOptions = {
+        from:
+          process.env.NODEMAILER_FROM_EMAIL +
+          "<" +
+          process.env.NODEMAILER_FROM_EMAIL +
+          ">",
+        to: mail_address,
+        subject: subject,
+        html: htmlToSend,
+        ...(attachments.length > 0 && attachments)
+      };
 
-        await transport.sendMail(mailOptions, function (error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(response)
-            }
-        });
-
-    } catch (error) {
-        logger.crit("crit", error, { service: 'email.js' });
-        console.log(error)
-        if (error) return { message: `Something Went Wrong!!! Error: ${error}`, status: false }
+      await transport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(response);
+        }
+      });
     }
-
-}
+  } catch (error) {
+    logger.crit("crit", error, { service: "email.js" });
+    console.log(error);
+    if (error)
+      return {
+        message: `Something Went Wrong!!! Error: ${error}`,
+        status: false,
+      };
+  }
+};
 
 module.exports = {
-    Mail
-}
+  Mail,
+};
--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
 const { po_activity_type } = require("../enums/po_enum");
+const { Mail } = require("../src/utils/email");
+const path = require('path');
 
 // Import ENUM for PO Activities
-console.log(po_activity_type.CREATE_PO)
+// console.log(po_activity_type.CREATE_PO)
+
+// Test Mailer
+Mail('sumonskys@gmail.com', "Test Subject", {"data": 1}, '', 100001, [{
+    filename: '37-1674769035302.pdf',
+    path: path.join(__dirname, '../tmp/37-1674769035302.pdf'),
+    contentType: 'application/pdf'
+}])

136bf101002484a663a9bfcbfedbd4950acd6607
Samrat Shahajahan <sumonskys@gmail.com>
2023-01-29 21:19:15 +0600

    Email Attachment Config

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const path = require('path');
 // console.log(po_activity_type.CREATE_PO)
 
 // Test Mailer
-Mail('sumonskys@gmail.com', "Test Subject", {"data": 1}, '', 100001, [{
+Mail('sumonskys@gmail.com', "Test Subject", {"data": 1}, 'allias', 100001, [{
     filename: '37-1674769035302.pdf',
     path: path.join(__dirname, '../tmp/37-1674769035302.pdf'),
     contentType: 'application/pdf'
